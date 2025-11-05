152fd01b99aba8ff1d55d0364ae7bf157e4d561c
IamShoe <sjoerd.aalbers@gmail.com>
2025-11-05 16:49:10 +0100

    float sound

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -6,7 +6,7 @@ public class MusicManager : MonoBehaviour
     public AudioSource slowStep;
     public AudioSource fastStep;
     public AudioSource mediumStep;
-
+    public AudioSource balloon;
     private AudioSource previousSong;
 
     public void ChangeSound(AudioSource newSound)
