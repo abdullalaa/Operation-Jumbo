656abfede9acbb859f51bb7baacbc7b2d943eef5
IamShoe <sjoerd.aalbers@gmail.com>
2025-11-06 13:33:23 +0100

    commitment issues

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -43,7 +43,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadCurrentScene();
+        }
     }
 
     // physics update
